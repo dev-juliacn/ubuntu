e6b94cef16f00d3486a4835441d512d618965869
Sam Liu <ontouchstart@gmail.com>
2018-08-17 15:35:54 -0400

    set package urls to https://github.com/juliacn forks

--- Makefile
+++ Makefile
@@ -12,7 +12,7 @@ bash:
 	docker run --rm -it $(NAMESPACE)/$(IMAGE):$(TAG) bash
 
 build:
-	docker build -t $(NAMESPACE)/$(IMAGE):$(TAG) . | tee docker.log
+	docker build --no-cache -t $(NAMESPACE)/$(IMAGE):$(TAG) . | tee docker.log
 
 test:
 	docker run --rm -it $(NAMESPACE)/$(IMAGE):$(TAG) julia test.jl | tee test.log
--- install.jl
+++ install.jl
@@ -1,10 +1,10 @@
 # package list from https://www.reddit.com/r/IAmA/comments/97jdb9/weve_spent_the_past_9_years_developing_a_new/e48olwm/
 using Pkg
-Pkg.add(Pkg.PackageSpec(url="https://github.com/timholy/Revise.jl"))
-Pkg.add(Pkg.PackageSpec(url="https://github.com/JuliaCI/BenchmarkTools.jl"))
-Pkg.add(Pkg.PackageSpec(url="https://github.com/JuliaArrays/StaticArrays.jl"))
-Pkg.add(Pkg.PackageSpec(url="https://github.com/Evizero/UnicodePlots.jl"))
-import Revise
-import BenchmarkTools
-import StaticArrays
-import UnicodePlots
+Pkg.add(Pkg.PackageSpec(url="https://github.com/juliacn/Revise.jl"))
+Pkg.add(Pkg.PackageSpec(url="https://github.com/juliacn/BenchmarkTools.jl"))
+Pkg.add(Pkg.PackageSpec(url="https://github.com/juliacn/StaticArrays.jl"))
+Pkg.add(Pkg.PackageSpec(url="https://github.com/juliacn/UnicodePlots.jl"))
+using Revise
+using BenchmarkTools
+using StaticArrays
+using UnicodePlots
