609eed5d4aa9e97c89172f56ffccab11cfb819e0
Sam Liu <ontouchstart@gmail.com>
2018-08-21 13:23:05 -0400

    pip install packages

--- install.jl
+++ install.jl
@@ -1,3 +1,9 @@
 using Pkg
+Pkg.add(Pkg.PackageSpec(url="https://github.com/JuliaPy/Conda.jl"))
 Pkg.add(Pkg.PackageSpec(url="https://github.com/JuliaPy/PyCall.jl"))
 using PyCall
+using Conda
+path = Conda.ROOTENV
+run(`$path/bin/pip install cython`)
+run(`$path/bin/pip install --upgrade pip`)
+run(`$path/bin/pip list`)
